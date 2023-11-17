042e753d33f8cbc509a73aee8e25de32fec8b1d3
SubhadipKundu1999 <subhadipkundu1000@gmail.com>
2023-11-17 14:36:24 +0530

    handle POST request and input validation using joi

--- RESTFul-Services/app.js
+++ RESTFul-Services/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
 const app = express();
+const Joi = require("joi");
+
+// middleware
+app.use(express.json());
 
 const port = process.env.PORT ||3000;
 
@@ -33,6 +36,58 @@ app.get('/api/posts/:year/:month',(req, res)=>{
 })
 app.get("/api/users/",(req, res)=>{
     res.send(req.query);
+})
+
+
+// courses array
+const courses =[
+    {
+        id:0,
+        name:'nodeJs'
+    },
+    {
+        id:1,
+        name:'mongoDB'
+    },
+    {
+        id:2,
+        name:'React'
+    }
+]
+
+// handling post requests
+
+app.post("/api/courses/",(req, res)=>{
+    // custom input validation
+       // if(!req.body.name || req.body.name.length<3){
+
+    //     res.status(400).send("name is required and should be minimum 3 character");
+    // }
+
+    // input validation using Joi npm package
+const schema = Joi.object({
+    name: Joi.string().min(3).required()
+})
+
+
+const result =schema.validate({name: req.body.name});
+console.log(result);
+
+
+if(result.error){
+    res.send(result.error);
+}
+    else{
+
+
+    const course={
+        id:courses.length+1,
+        name:req.body.name,
+    }
+    courses.push(course);
+    res.send(course);
+}
+
 })
 
 
