2531dc0ed1601bb269f168ea924db3cb71795787
SubhadipKundu1999 <subhadipkundu1000@gmail.com>
2023-11-21 23:14:36 +0530

    mongoose validation

--- mongo-demo/app.js
+++ mongo-demo/app.js
@@ -7,28 +7,70 @@ const mongoose = require("mongoose");
 
 
 
- //schema
+ // Schema //
+
  const courseSchema =new mongoose.Schema({
-    name:String,
-    author:String,
-    tags:[String],
+    name:{
+      type:String, 
+      required:[true, "Name field is required"],
+      minLength:3,
+      maxLength:100
+    },
+    author:{
+      type:String,
+      required:[ true, "Author name is required"]
+    },
+    tags:{
+      type:Array,
+      // custom validator
+      validate:{
+        validator: function(v){
+          return v.length > 0 ;
+        },
+        message:'A course should have at least one tag.'
+
+      }
+
+    },
+
+    category:{
+      type:String,
+      required:[true, "category field is required with one of the value -> web mobile, network "],
+      enum:['web', 'mobile', 'network'] 
+    },
     date:{type:Date, default:Date.now},
-    isPublished:Boolean
+    isPublished:Boolean,
+    price:{
+      type:Number,
+      required: function(){ return this.isPublished} //price is required when course is published
+    }
  })
 
-// creating a model:
+
+ // creating a model:
 const Course = mongoose.model('Course', courseSchema);
 
+ async function createCourse(){
+   const course = new Course({
+    name:"csS",
+    tags:['frontend', 'design'],
+    author:'bristi Kundu',
+    category :'web',
+    isPublished:false,
+    price:120
+   });
+
+   try{
+    const result =await course.save();
+    console.log(result);
+   }
+   catch(err){
+    console.log(err.message);
+   }
+ } 
+
+ createCourse();
 
 
-//update course
 
-async function DeleteCourse(id){
-
-  const result = await Course.deleteOne({author:'Subhadip'});
-  console.log(result)
-}
-const id = '655c9b0423979af8a98f52f'
-
-DeleteCourse(id);
 
