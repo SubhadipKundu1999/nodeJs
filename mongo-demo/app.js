4ef7836738bba04d2a882445fa41a2dc7b82803e
SubhadipKundu1999 <subhadipkundu1000@gmail.com>
2023-11-21 17:29:48 +0530

    creating a schema , coverting into a model, creating a course document and save it

--- mongo-demo/app.js
+++ mongo-demo/app.js
@@ -5,3 +5,35 @@ const mongoose = require("mongoose");
  mongoose.connect("mongodb://0.0.0.0:27017/playground")
  .then(()=>console.log("connected to mongodb"))
  .catch((error)=>console.log("could not connect to mongodb", error))
+
+
+
+ //schema
+
+ const courseSchema =new mongoose.Schema({
+    name:String,
+    author:String,
+    tags:[String],
+    date:{type:Date, default:Date.now},
+    isPublished:Boolean
+ })
+
+// creating a model:
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse(){
+
+    // creating a course document
+const course = new Course({
+    name:"Node js Course",
+    author:"Mosh",
+    tags:["node", "backend"],
+    isPublished:true
+})
+
+// saveing a document 
+const result = await course.save();
+console.log(result);
+}
+createCourse();
+
