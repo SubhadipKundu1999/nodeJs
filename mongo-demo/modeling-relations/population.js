2eb502cd91f3ede2f6befc89f05def5f600090b1
Subhadip Kundu <subhadipkundu1000@gmail.com>
2023-11-23 10:46:43 +0530

    modeling relationship by referenceing document

---
+++ mongo-demo/modeling-relations/population.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://0.0.0.0:27017/playground")
+.then(()=>console.log("connected to mongodb"))
+.catch((error)=>console.log("could not connect to mongodb", error))
+
+
+const Author = mongoose.model('Author', new mongoose.Schema({
+    name:String,
+    bio:String,
+    website: String
+}));
+
+const Course = mongoose.model( 'Course', new mongoose.Schema({
+    name: String,
+    author:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Author'
+    }
+}))
+
+
+async function createAuthor(name, bio, website){
+    const author = new Author({
+        name, 
+        bio, 
+        website
+    });
+
+    const result = await author.save();
+    console.log(result);
+}
+
+async function createCourse(name, author){
+    const course = new Course({
+        name, 
+        author
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+async function listCourse(){
+    const course = await Course
+    .find()
+    .select('name');
+
+    console.log(course);
+
+}
+
+// createAuthor('subha', 'my bio', 'my website');
+
+// createCourse('my course 2', "655edc01ca957516ac26a878")
+
+listCourse();
