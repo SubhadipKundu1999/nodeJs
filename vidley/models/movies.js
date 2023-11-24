4abc8eb63037df89913ede826a350c91dfea74cd
Subhadip Kundu <subhadipkundu1000@gmail.com>
2023-11-24 17:25:02 +0530

    refactoring to apply joi.objectId() to all models

--- vidley/models/movies.js
+++ vidley/models/movies.js
@@ -36,7 +36,7 @@ function validateMovie(Movie){
 
     const schema = Joi.object({
         title: Joi.string().min(5).max(50).required(),
-        genreId: Joi.string().required(),
+        genreId: Joi.objectId().required(),
         numberInStock: Joi.number().min(0).required(),
         dailyRentalRate:Joi.number().min(0).required()
     })
