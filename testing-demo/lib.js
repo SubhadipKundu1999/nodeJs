961734d9b6da5a18f473f57c71fefd8e71493402
Subhadip Kundu <subhadipkundu1000@gmail.com>
2023-11-30 11:06:15 +0530

    Testing strings

--- testing-demo/lib.js
+++ testing-demo/lib.js
@@ -1,15 +1,15 @@
 // const db = require("./db");
-// const mail = require("./mail");
+// // const mail = require("./mail");
 
 // Testing numbers
 module.exports.absolute = function(number) {
   return number >= 0 ? number : -number;
 };
 
-// // Testing strings
-// module.exports.greet = function(name) {
-//   return "Welcome " + name;
-// };
+// Testing strings
+module.exports.greet = function(name) {
+  return "Welcome " + name +'!';
+};
 
 // // Testing arrays
 // module.exports.getCurrencies = function() {
